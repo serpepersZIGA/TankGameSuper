0fa75c76221dd9d0e175033e19fbbf9643aa9a66
danii <daniilfedoskin8@gmail.com>
2025-06-16 16:43:37 +0300

    MapBuildingUpdate

--- core/src/com/mygdx/game/build/Build/MapSpawner.java
+++ core/src/com/mygdx/game/build/Build/MapSpawner.java
@@ -7,28 +7,18 @@ import com.mygdx.game.main.Main;
 public class MapSpawner extends Building {
     public MapSpawner(int x, int y,byte team) {
         super(x,y,team);
-        this.x = x;
-        this.y = y;
-        this.team = team;
-        this.SpawnMaxTime = 600;
-        RenderBuilding = Main.BuildingRegister.Update_big_build_wood1;
-        ConstructBuilding = new boolean[][]{{false}};
-
+        //ListFunc
         super.Data();
-
-
     }
 
     @Override final
     public void all_action() {
         super.all_action();
-        super.SpawnUnit();
+        //super.SpawnUnit();
 
     }
 
-    @Override
-    final
+    @Override final
     public void update() {
-        RenderBuilding.render(this.x, this.y, this.width_render, this.height_render);
     }
 }
